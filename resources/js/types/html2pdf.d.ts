36bbbf5f6c5aad02384eb5fa50f72e074994fb45
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-05-16 20:33:29 +0700

    fix: lint error

--- resources/js/types/html2pdf.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'html2pdf.js' {
-    export default function(): {
-        set: (options: any) => any;
-        from: (element: HTMLElement) => any;
-        save: () => Promise<void>;
-    };
-}
