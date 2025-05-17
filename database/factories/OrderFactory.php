d3787f18343446bf3ee25e579f9ea11f3a4337ac
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-05-17 15:49:31 +0700

    refactor(database): create more orders

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -9,35 +9,35 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class OrderFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        $totalAmount = $this->faker->numberBetween(200, 5000) * 1000;
-        $discountAmount = $this->faker->optional(0.3, 0)->numberBetween(50, 200) * 1000;
-        $finalAmount = $totalAmount - $discountAmount;
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    $totalAmount = $this->faker->numberBetween(500, 10000) * 1000; // Tổng tiền đơn hàng từ 500k đến 10M
+    $discountAmount = $this->faker->numberBetween(0, 5) * $totalAmount / 100; // Số tiền giảm giá từ 0-5% tổng tiền
+    $finalAmount = $totalAmount - $discountAmount;
 
-        return [
-            'order_date' => $this->faker->dateTimeBetween('-3 months', 'now'),
-            'total_amount' => $totalAmount,
-            'discount_amount' => $discountAmount,
-            'final_amount' => $finalAmount,
-            'payment_method' => $this->faker->randomElement(['cash', 'card', 'transfer']),
-            'status' => $this->faker->randomElement(['completed', 'canceled', 'pending']),
-        ];
-    }
+    return [
+      'order_date' => $this->faker->dateTimeBetween('-6 months', 'now'),
+      'total_amount' => $totalAmount,
+      'discount_amount' => $discountAmount,
+      'final_amount' => $finalAmount,
+      'payment_method' => $this->faker->randomElement(['cash', 'card', 'transfer']),
+      'status' => $this->faker->randomElement(['completed', 'canceled', 'pending']),
+    ];
+  }
 
-    /**
-     * Configure the model factory.
-     */
-    public function configure()
-    {
-        return $this->afterMaking(function (\App\Models\Order $order) {
-            // Định nghĩa id theo Snowflake
-            $order->id = app('snowflake')->id();
-        });
-    }
+  /**
+   * Configure the model factory.
+   */
+  public function configure()
+  {
+    return $this->afterMaking(function (\App\Models\Order $order) {
+      // Định nghĩa id theo Snowflake
+      $order->id = app('snowflake')->id();
+    });
+  }
 }
